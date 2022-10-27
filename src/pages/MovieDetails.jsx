c79442996d5056c420a86d729c0f88f17893534a
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-28 01:14:38 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,6 @@
-import { useParams } from "react-router-dom";
+/* import { useParams } from "react-router-dom";
 
 const MovieDetails = () => {
   const { productId } = useParams();
   return <div>Now showing product with id - {productId}</div>;
-};
+}; */
