6b8d7e105d86dc165a636f1ebd682071dd3a9b75
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-29 02:16:00 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
-export const Home = () => {
-    return (
-      <main>
-        <h1>Welcome</h1>
-        <img src="https://via.placeholder.com/960x240" alt="" />
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
-          laboriosam placeat incidunt rem illum animi nemo quibusdam quia
-          voluptatum voluptate.
-        </p>
-      </main>
-    );
-  };
+import { useState, useEffect } from "react";
+import { getTrendingMovies } from "services/api";
+import { TrendingMovieList } from "components/TrendingMovieList/TrendingMovieList";
+import { HomeTitle } from './Home.styled';
+
+const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  
+  useEffect(() => {
+      getTrendingMovies().then(setTrendingMovies);
+  }, []);
+  
+  return <section>
+      <HomeTitle>Trending today</HomeTitle>
+      <TrendingMovieList movies={trendingMovies} link={`movies/`}/>
+  </section>
+}
+
+export default Home;
   
