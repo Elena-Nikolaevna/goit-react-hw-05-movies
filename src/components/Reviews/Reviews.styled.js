51cd64f9a8f319347c36b28c3b3b27123dc177a3
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-29 02:15:46 +0300

    Create Reviews.styled.js

---
+++ src/components/Reviews/Reviews.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ReviewsText = styled.p`
+    color: black;
+`;
+
+export const ReviewsList = styled.ul`
+    list-style-type: none;
+    margin-top: 20px;
+`;
+
+export const ReviewsListItem = styled.li`
+`;
+
+export const ReviewsContent = styled.p`
+    max-width: auto;
+    margin-right: 20px;
+    padding: 5px;
+    border: 1px grey solid;
+    box-shadow: 4px 4px 4px 2px rgba(0, 0, 0, 0.3);
+`;
