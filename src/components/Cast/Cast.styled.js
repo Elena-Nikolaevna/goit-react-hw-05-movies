54a987bba4f151a41867999a1c5d06f6664174f3
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-29 02:13:36 +0300

    Create Cast.styled.js

---
+++ src/components/Cast/Cast.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CastList = styled.ul`
+  list-style: none;
+  padding-left: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+export const CastListItem = styled.li`
+  border: 1px gray solid;
+  padding: 5px;  
+  text-align: center;
+`;
