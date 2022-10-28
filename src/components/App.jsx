ed8fe612e1f4bf5684034ef29e5c9e6830c0079d
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-29 02:13:28 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,51 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-import { Home } from '../pages/Home';
-import { Movies } from '../pages/Movies';
-import {NotFound} from '../pages/NotFound';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy } from "react";
+//import styled from 'styled-components';
+//import { Home } from '../pages/Home';
+//import { Movies } from '../pages/Movies';
+//import { NotFound } from '../pages/NotFound';
+import { Container } from './App.styled';
+import { Layout } from "./Layout/Layout";
+import { Cast } from "./Cast/Cast";
+import { Reviews } from "./Reviews/Reviews";
+
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import("../pages/Movies"));
+const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+
 //import {MovieDetails} from '../pages/MovieDetails'
 
-
-const StyledLink = styled(NavLink)`
+/* const StyledLink = styled(NavLink)`
   color: black;
 
   &.active {
     color: red;
   }
-`;
+`; */
 
 export const App = () => {
   return (
-    <div>
-      <nav>
-        <StyledLink to="/" end>
-          Home
-        </StyledLink>
-        <StyledLink to="/movies">Movies</StyledLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        {/* <Route puth="/movies/:movieId" element={<MovieDetails />} /> */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <Container>
+     {/*  <Header>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header> */}
+
+<Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />}/>
+        <Route path='movies' element={<Movies />}/>
+        <Route path='movies/:movieId' element={<MovieDetails />}> 
+          <Route path='cast' element={<Cast />}/>
+          <Route path='reviews' element={<Reviews />}/>
+        </Route>
+        <Route path="*" element={<Navigate to="/"/>}/>
+      </Route>
+    </Routes>
+    </Container>
   );
 };
