4797303cd4211e0948941d14b7c3062a5516272a
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-29 02:15:42 +0300

    Create MoviesSearchForm.styled.js

---
+++ src/components/MoviesSearchForm/MoviesSearchForm.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  margin-top: 20px;
+  margin-left: 10px;
+  border-radius: 5px;
+  
+`;
+
+export const FormInput = styled.input`
+  width: 200px;
+  margin-right: 10px;
+`;
+
+export const FormBtn = styled.button`
+  
+`;
