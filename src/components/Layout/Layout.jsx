09c77d4e021f9d79333d74ea6f423fe711702016
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-29 03:09:57 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,10 +3,10 @@ import { Outlet } from "react-router-dom";
 import { HomePage } from "../HomePage/HomePage";
 
 export const Layout = () => {
-    return <main>
+    return <div>
         <HomePage />
         <Suspense fallback={<div>Loading...</div>}>
             <Outlet />
         </Suspense>
-    </main>
+    </div>
 }
