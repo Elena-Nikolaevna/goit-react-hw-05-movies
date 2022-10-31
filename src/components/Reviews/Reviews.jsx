719f25f34afd14cadd24e3fe421a6a2918a1c140
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-01 00:45:47 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,32 +1,39 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getMovieReviews } from "services/api";
-import { ReviewsText, ReviewsList, ReviewsListItem, ReviewsContent } from './Reviews.styled';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieReviews } from 'services/api';
+import {
+  ReviewsText,
+  ReviewsList,
+  ReviewsListItem,
+  ReviewsContent,
+} from './Reviews.styled';
 
-export const Reviews = () => {
-    const {movieId} = useParams();
-    const [reviews, setReviews] = useState([]);
+const Reviews = () => {
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
 
-    useEffect(() => {
-        getMovieReviews(movieId).then(setReviews)
-    }, [movieId]);
+  useEffect(() => {
+    getMovieReviews(movieId).then(setReviews);
+  }, [movieId]);
 
-    const isReviewsEmpty = reviews.length === 0;
-    
-    return (
-        isReviewsEmpty ? 
-        <ReviewsText>We don't have any reviews for this movie</ReviewsText>
-        :
-        <ReviewsList>
-            {reviews.map(review => {
-                const {id, content, author} = review;
-                return (
-                    <ReviewsListItem key={id}>
-                        <ReviewsText><b>Author: {author}</b></ReviewsText>
-                        <ReviewsContent>{content}</ReviewsContent>
-                    </ReviewsListItem>
-                )
-            })}
-        </ReviewsList>
-    )
-}
+  const isReviewsEmpty = reviews.length === 0;
+
+  return isReviewsEmpty ? (
+    <ReviewsText>We don't have any reviews for this movie</ReviewsText>
+  ) : (
+    <ReviewsList>
+      {reviews.map(review => {
+        const { id, content, author } = review;
+        return (
+          <ReviewsListItem key={id}>
+            <ReviewsText>
+              <b>Author: {author}</b>
+            </ReviewsText>
+            <ReviewsContent>{content}</ReviewsContent>
+          </ReviewsListItem>
+        );
+      })}
+    </ReviewsList>
+  );
+};
+export default Reviews;
