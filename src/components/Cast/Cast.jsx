02af1a0c73247e52499179e1e70389d7fc0ee8e7
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-01 00:45:44 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,33 +1,36 @@
-import { useEffect,  useState } from "react";
-import { useParams } from "react-router-dom";
-import { getMovieCast } from "services/api";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieCast } from 'services/api';
 import { CastList, CastListItem } from './Cast.styled';
 
-const BASE_PHOTO_URL = "https://image.tmdb.org/t/p/w200/";
+const BASE_PHOTO_URL = 'https://image.tmdb.org/t/p/w200/';
 
-export const Cast = () => {
-    const {movieId} = useParams();
-    const [castInfo, setCastInfo] = useState([]);
+const Cast = () => {
+  const { movieId } = useParams();
+  const [castInfo, setCastInfo] = useState([]);
 
-    useEffect(() => {
-        getMovieCast(movieId).then(setCastInfo);
-    }, [movieId]);
+  useEffect(() => {
+    getMovieCast(movieId).then(setCastInfo);
+  }, [movieId]);
 
-    if (castInfo.length === 0) {
-        return null;
-    }
+  if (castInfo.length === 0) {
+    return null;
+  }
 
-    return (
-        <CastList>
-            {castInfo.map(actor => {
-                const {name, character, profile_path, id} = actor;
+  return (
+    <CastList>
+      {castInfo.map(actor => {
+        const { name, character, profile_path, id } = actor;
 
-                return <CastListItem key={id}>
-                    <img src={BASE_PHOTO_URL + profile_path} alt={name}/>
-                    <p>{name}</p>
-                    <p>Character: {character}</p>
-                </CastListItem>
-            })}
-        </CastList>
-    )
-}
+        return (
+          <CastListItem key={id}>
+            <img src={BASE_PHOTO_URL + profile_path} alt={name} />
+            <p>{name}</p>
+            <p>Character: {character}</p>
+          </CastListItem>
+        );
+      })}
+    </CastList>
+  );
+};
+export default Cast;
